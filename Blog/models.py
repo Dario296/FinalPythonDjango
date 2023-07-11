39ceff9e8c09b23c3dbca27d471a4465573a32e0
Dario296 <dariorodriguez296@yahoo.com.ar>
2023-07-10 21:12:07 -0300

    Creo modelo blog

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length = 30)
+    subtitulo = models.CharField(max_length = 50)
+    cuerpo = models.TextField(default = '')
+    imagen = models.ImageField()
+    fecha = models.DateField(auto_now_add = True)
+    autor = models.OneToOneField(User, on_delete = models.CASCADE)
